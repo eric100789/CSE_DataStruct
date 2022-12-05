a6dc6328bbd9880d21469262eff92abe8fea125d
eric100789 <eric100789@yahoo.com.tw>
2022-12-06 02:03:24 +0800

    complete(only huffman)

--- 作業8/huffman2.cpp
+++ 作業8/huffman2.cpp
@@ -24,9 +24,9 @@ class Node
     private:
     int num;
     int val;
-    bool isadd;
     Node * right;
     Node * left;
+    string str;
 
     public:
     friend class Tree;
@@ -35,23 +35,25 @@ class Node
         this->num = 0;
         this->right = nullptr;
         this->left = nullptr;
-        isadd = false;
+        str = "";
     }
-    Node(int val, int num, bool isAdded)
+    Node(int val, int num)
     {
         this->val = val;
         this->num = num;
         this->right = nullptr;
         this->left = nullptr;
-        this->isadd = isAdded;
+        this->str = "";
+        str += (char)val;
     }
-    Node(int val, int num, Node * right, Node * left, bool isAdded)
+    Node(int val, int num, Node * right, Node * left)
     {
         this->val = val;
         this->num = num;
         this->right = right;
         this->left = left;
-        this->isadd = isAdded;
+        this->str = "";
+        this->str += (char)val;
     }
     Node(Node * right, Node * left)
     {
@@ -59,7 +61,8 @@ class Node
         this->num = right->num + left->num;
         this->right = right;
         this->left = left;
-        this->isadd = true;
+        this->str += (char)right->val;
+        this->str += (char)left->val;
     }
     int getNum()
     {
@@ -68,6 +71,10 @@ class Node
     int getVal()
     {
         return val;
+    }
+    string getStr()
+    {
+        return str;
     }
     void makeEncoder(vector<Encoder> & target, string now="")
     {
@@ -80,11 +87,22 @@ class Node
         }
         else
         {
-            
+            if(this->left)
             this->left->makeEncoder(target, now+"0");
+            if(this->right)
             this->right->makeEncoder(target, now+"1");
             
         }
+    }
+    void debug()
+    {
+        if(this->left == nullptr && this->right == nullptr)
+        {
+            //cout << "My:" << this->val << "/" << (char)this->val << "/num" << this->num << endl;
+            return;
+        }
+        if(this->left) this->left->debug();
+        if(this->right) this->right->debug();
     }
 };
 
@@ -151,7 +169,8 @@ void huff_encode(char * file_path)
     {
         if(i_arr[i] > 0)
         {
-            vec_node.push_back(new Node(i-128,i_arr[i],false));
+            //cout << "Hey:" << (char)(i-128) << " has " << i_arr[i] << endl;
+            vec_node.push_back(new Node(i-128,i_arr[i]));
         }
     }
     sort(vec_node.begin(),vec_node.end(), compare );
@@ -164,6 +183,7 @@ void huff_encode(char * file_path)
         sort(vec_node.begin(),vec_node.end(), compare );
         //delete temp1,temp2;
     }
+    vec_node[0]->debug();
     vector<Encoder> huffmaned;
     vec_node[0]->makeEncoder(huffmaned);
 
@@ -176,7 +196,7 @@ void huff_encode(char * file_path)
 
     ofstream outputFile("output.txt", ios::out | ios::binary);
 
-    outputFile << huffmaned.size() << '\n';
+    outputFile << huffmaned.size() << ' ' << text.size() << '\n';
     for(int i=0 ; i<huffmaned.size() ; i++)
     {
         outputFile << huffmaned[i].prenum << ' ' << huffmaned[i].lastnum.size() << ' ';
@@ -201,6 +221,7 @@ void huff_encode(char * file_path)
         outputFile << '\n';
     }
 
+    char tmp = 0, cnt = 0;
     for(int i=0 ; i<text.size() ; i++)
     {
         int index;
@@ -209,11 +230,11 @@ void huff_encode(char * file_path)
             if(huffmaned[j].prenum == text[i])
             {
                 index = j;
+                //cout << huffmaned[j].lastnum;
                 break;
             }
         }
 
-        char tmp = 0, cnt = 0;
         for(int j=0 ; j<huffmaned[index].lastnum.size() ; j++)
         {
             tmp <<= 1;
@@ -225,13 +246,12 @@ void huff_encode(char * file_path)
                 tmp = cnt = 0;
             }
         }
-        if (tmp)
-        {
-            tmp <<= (8 - cnt);
-            outputFile << tmp;
-        }
     }
-
+    if (tmp)
+    {
+        tmp <<= (8 - cnt);
+        outputFile << tmp;
+    }
 
     outputFile.close();
     inputFile.close();
@@ -239,10 +259,10 @@ void huff_encode(char * file_path)
 
 void huff_decode(char * file_path)
 {
-    cout << "---------"<<endl;
+    //cout << "---------"<<endl;
     ifstream inputFile(file_path, ios::in | ios::binary);
     ofstream outputFile("unzipped.txt", ios::out | ios::binary);
-    int decode_num; inputFile >> decode_num;
+    int decode_num,text_num; inputFile >> decode_num >> text_num;
     vector<Decoder> huffmaned;
     for(int i=0 ; i<decode_num ; i++)
     {
@@ -258,11 +278,12 @@ void huff_decode(char * file_path)
         }
         temp.lastnum = convert_string(get_str,temp.len);
         huffmaned.push_back(temp);
-        cout<< "here:" << temp.prenum << "/" << temp.len << "/" << temp.lastnum << endl;
+        //cout<< "here:" << temp.prenum << "/" << temp.len << "/" << temp.lastnum << endl;
     }
     inputFile.get();
     char ch;
     int cnt=0;
+    int nowch = 0;
     string ans = "";  
     while(~(ch=inputFile.get()))
     {
@@ -272,9 +293,11 @@ void huff_decode(char * file_path)
             else ans += '0';
             for(int j=0; j<huffmaned.size() ; j++)
             {
-                if(huffmaned[j].lastnum == ans)
+                if(huffmaned[j].lastnum == ans )
                 {
-                    cout << huffmaned[j].lastnum << "/" << ans << "/" << huffmaned[j].prenum << "/" << (char)huffmaned[j].prenum << endl;
+                    if(nowch > text_num) goto STOP;
+                    nowch++;
+                    //cout << huffmaned[j].lastnum << "/" << ans << "/" << huffmaned[j].prenum << "/" << (char)huffmaned[j].prenum << endl;
                     outputFile << (char)huffmaned[j].prenum;
                     ans = "";
                     break;
@@ -282,6 +305,7 @@ void huff_decode(char * file_path)
             }
         }
     }
+    STOP:
     outputFile.close();
     inputFile.close();
 }
